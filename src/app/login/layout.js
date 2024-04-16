5059c6e3decea22009e32c31278cd4bc330fa64a
marmikj <140173006+marmikj@users.noreply.github.com>
2024-04-16 14:24:29 +0530

    layout changes

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,24 +1,31 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import "./login.css";
+import { usePathname } from "next/navigation";
 
 const layout = ({ children }) => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <div>
-      <ul className="login-menu">
-        <li>
+      {pathname != "/login/loginteacher" ? 
+        <ul className="login-menu">
+          <li>
             <h4>Login Navbar</h4>
-        </li>
-        <li>
-          <Link href="/login">login main</Link>
-        </li>
-        <li>
-          <Link href="/login/loginstudent">login Student</Link>
-        </li>
-        <li>
-          <Link href="/login/loginteacher">login Techer</Link>
-        </li>
-      </ul>
+          </li>
+          <li>
+            <Link href="/login">login main</Link>
+          </li>
+          <li>
+            <Link href="/login/loginstudent">login Student</Link>
+          </li>
+          <li>
+            <Link href="/login/loginteacher">login Techer</Link>
+          </li>
+        </ul>
+
+       : <Link href='/login'>Go To Login PAge </Link>}
       {children}
     </div>
   );
